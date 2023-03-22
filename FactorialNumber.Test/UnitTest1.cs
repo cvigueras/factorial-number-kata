eff7397ee9c53df54e6750d453a477ab763345fc
cvigueras <carlosj.vigueras@gmail.com>
2023-03-22 17:05:40 +0100

    Chore: Init project

---
+++ FactorialNumber.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FactorialNumber.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
