df9fbacba79cd5bb98a5a2bcb310204d509b18cd
cvigueras <carlosj.vigueras@gmail.com>
2023-03-22 17:21:29 +0100

    Refactor: Method GetResult in production code.

--- FactorialNumber.Console/Factorial.cs
+++ FactorialNumber.Console/Factorial.cs
@@ -4,11 +4,6 @@ public class Factorial
 {
     public static object GetResult(int number)
     {
-        if (number == 0)
-        {
-            return 0;
-        }
-
         if (number < 0)
         {
             return 0;
